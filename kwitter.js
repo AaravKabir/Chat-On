541472578b0503d0fecafccbcb3c6419366d8804
AaravKabir <75346593+AaravKabir@users.noreply.github.com>
2021-11-02 22:03:10 -0400

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -1,7 +1,5 @@
 function add_user() {
     user_name = document.getElementById("user_name").value;
     localStorage.setItem("user_name", user_name);
-    alert(1);
     window.location="kwitter_room.html";
-    return false;
 } 
